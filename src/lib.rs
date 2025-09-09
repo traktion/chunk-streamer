6b1d51ea42ecc02cfd9fc25b8d6d74cc698a7004
Paul Green <traktion0@gmail.com>
2025-09-09 15:23:36 +0100

    - Remove exports for internal dependencies - Bump version number

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,3 @@ pub mod chunk_streamer;
 mod chunk_fetcher;
 pub mod chunk_receiver;
 mod chunk_sender;
-
-pub use autonomi;
-pub use bytes;
-pub use log;
-pub use self_encryption;
-pub use tokio;
-pub use futures;
-pub use futures_util;
